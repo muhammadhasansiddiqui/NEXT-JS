60f4329d07ba43547568655887819b58e6296c73
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-09-21 02:50:24 +0500

    next js

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,11 +1,17 @@
 import Image from "next/image";
 
+import { useState } from "react";
+
 export default function Home() {
+
+  const  [x,setX] =useState(0)
+  const  [y,sety] =useState(0)
+
   return (
     <div className="flex items-center justify-center h-screen bg-slate-950 ">
 
-      <div className="shadow-xl bg-yellow-500 ">
-      <h1 className="text-3xl font-bold text-center text-black ">Hello World</h1>
+      <div className=" {`h-[100vh]`} style= {{backgroundColor : `rgb(${x}, ${y/x - 50})} onMouseMove={} }">
+      <h1 className="text-3xl font-bold text-center text-black ">Hello World  !</h1>
       </div>
     </div>
   );
