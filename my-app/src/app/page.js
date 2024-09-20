340b4caa48fc753462b59273651ecd8837990d4a
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-09-20 15:00:08 +0500

    next js

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-slate-950 ">
-      <h1 className="text-3xl font-bold text-center text-yellow-100">Hello World</h1>
+
+      <div className="shadow-xl bg-yellow-500 ">
+      <h1 className="text-3xl font-bold text-center text-black ">Hello World</h1>
+      </div>
     </div>
   );
 }
